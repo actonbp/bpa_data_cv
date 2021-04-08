bda2754392165ce02ce5b5bf7a676880a91ce842
actonbp <40258526+actonbp@users.noreply.github.com>
2021-04-08 15:04:14 -0400

    major update 4/8

---
+++ main.R
@@ -0,0 +1,12 @@
+#devtools::install_github("nstrayer/datadrivencv")
+
+#open "cv.rmd"
+
+setwd("C:/Users/bacto/OneDrive/Desktop/Programming/R/bpa_data_cv")
+#icons::icon
+
+.libPaths()
+
+#install.packages("C:/Program Files/R/R-4.0.3/library/datadrivencv", repos = NULL, type="source")
+
+#install.packages("C:/Users/bacto/OneDrive/Desktop/Programming/vsc/datadrivencv", repos = NULL, type="source")
---
+++ setup_with_ddcv.R
@@ -0,0 +1,11 @@
+library(datadrivencv)
+library(here)
+use_datadriven_cv(
+  full_name = "Bryan Acton",
+  data_location = "C:/Users/bacto/OneDrive/Desktop/Programming/R/bpa_data_cv",
+  pdf_location = "C:/Users/bacto/OneDrive/Desktop/Programming/R/bpa_data_cv/strayer_cv.pdf",
+  html_location = "nickstrayer.me/cv/",
+  source_location = "https://github.com/nstrayer/cv",
+  output_dir = here(),
+  open_files = FALSE
+)
