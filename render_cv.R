7cc772bc1b1143cb858a297e687cca0c6d05157e
actonbp <40258526+actonbp@users.noreply.github.com>
2021-04-14 14:47:59 -0400

    move old unused files away

--- render_cv.R
+++ render_cv.R
@@ -6,16 +6,18 @@
 # for the HTML and PDF rendering. This exercise is left to the reader.
 
 # Knit the HTML version
-rmarkdown::render("cv.rmd",
+rmarkdown::render("cv_current.rmd",
                   params = list(pdf_mode = FALSE),
-                  output_file = "cv.html")
+                  output_file = "Acton_CV_4.14.21.html")
 
 # Knit the PDF version to temporary html location
 tmp_html_cv_loc <- fs::file_temp(ext = ".html")
-rmarkdown::render("cv.rmd",
+rmarkdown::render("cv_current.rmd",
                   params = list(pdf_mode = TRUE),
                   output_file = tmp_html_cv_loc)
 
 # Convert to PDF using Pagedown
 pagedown::chrome_print(input = tmp_html_cv_loc,
-                       output = "cv.pdf")
+                       output = "Acton_CV_4.14.21.pdf")
+
+##CHANGE DATE IN FILE NAME WOOHOO!
