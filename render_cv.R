92856c070770cbb991ff93816896481b88588ebb
Bryan Acton <actonbp@gmail.com>
2023-10-09 13:14:06 -0400

    major oct 2023 updates
    - Fixed sheets package
    - fixed google fonts error
    - updated publications, teaching, current projects, etc
    - removed bar graph on the side

--- render_cv.R
+++ render_cv.R
@@ -4,11 +4,11 @@
 # this script to cache a version of the CV_Printer class with data already
 # loaded and load the cached version in the .Rmd instead of re-fetching it twice
 # for the HTML and PDF rendering. This exercise is left to the reader.
-
+library(datadrivencv)
 # Knit the HTML version
 rmarkdown::render("cv_current.rmd",
                   params = list(pdf_mode = FALSE),
-                  output_file = "Acton_CV_4.14.21.html")
+                  output_file = "Acton_CV_10.9.2023.html")
 
 # Knit the PDF version to temporary html location
 tmp_html_cv_loc <- fs::file_temp(ext = ".html")
@@ -18,6 +18,6 @@ rmarkdown::render("cv_current.rmd",
 
 # Convert to PDF using Pagedown
 pagedown::chrome_print(input = tmp_html_cv_loc,
-                       output = "Acton_CV_4.14.21.pdf")
+                       output = "Acton_CV_10.9.2023.pdf")
 
 ##CHANGE DATE IN FILE NAME WOOHOO!
